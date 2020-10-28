44d3923c554fbc822ea4438214724350cee7f8df
Tamara-Badalyan22 <70955345+Tamara-Badalyan22@users.noreply.github.com>
2020-10-29 00:24:24 +0400

    Add files via upload

---
+++ src/first/Lesson_1.java
@@ -0,0 +1,17 @@
+package first;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        byte a = 50;
+        System.out.println(a);
+        int b = 125;
+        System.out.println(b);
+        double c = 1.25;
+        System.out.println(c);
+        long l = 2134564687987653l;
+        System.out.println(l);
+        boolean t = true;
+        String s = "abcd";
+    }
+}
